1caab722a45241ebee18e255da4d2a1c96bd105d
emanuele <emanuele@system76-pc.localdomain>
2020-02-01 15:27:40 -0500

    added all cashman competitors

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,14 +1,9 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-	space: '4uybl1re8fu3',
-	accessToken: 'gir85I8dtK_mW1wbTr2-Mxy9LCNpFVBkfewG1AFvZjc'
+	space: process.env.REACT_APP_API_SPACE,
+	accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
-
-// export default createClient({
-// 	space: 'wjjznutuk0jw',
-// 	accessToken: 'GtINH9_14j4KjWJXzgNAmHmOZaCViH3UCW8h6-l3maw'
-// });
 
 // space: process.env.REACT_APP_API_SPACE,
 // accessToken: process.env.REACT_APP_ACCESS_TOKEN
